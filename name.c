5bf31c973775370b98a55b7b6f838df90c42c6e8
tejaschide <chidetejas@gmail.com>
2026-09-16 14:34:56 +0530

    Add basic name input and greeting functionality

---
+++ name.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+void main(){
+
+char name[20];
+
+clrscr();
+
+printf("Enter your name name;");
+
+scanf("%20s",name);
+
+printf("Hello ROBOTICS \n");
+
+printf("Welcome %s\n",name);
+
+getch();
+
+}
