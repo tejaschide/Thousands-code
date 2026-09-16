8992181d937ae544a17be3e82be46008d3e62d43
tejaschide <chidetejas@gmail.com>
2026-09-16 14:36:15 +0530

    Add program to calculate subtraction of two numbers

---
+++ difference.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, subtraction;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    subtraction = a - b;
+
+    printf("Subtraction = %d", subtraction);
+
+    return 0;
+}
